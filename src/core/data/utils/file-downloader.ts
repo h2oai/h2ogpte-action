ce78f3f920a947f0ab1d113e7591d9b699505d9f
Julian Garratt <julian.garratt@h2o.ai>
2025-06-19 09:06:42 +1000

    bug: added github token authorisation to attachment download

--- src/core/data/utils/file-downloader.ts
+++ src/core/data/utils/file-downloader.ts
@@ -207,6 +207,7 @@ export async function downloadCommentAttachments(
 
             const response = await fetch(signedUrl, {
               headers: {
+                'Authorisation': `token ${rest.auth}`,
                 'User-Agent': 'Mozilla/5.0 (compatible; GitHub-Attachment-Downloader/1.0)',
                 'Accept': '*/*',
               },
