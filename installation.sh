0f87abaca722e27637a4ea4bd7fefd4561f0088e
Issac Liu <issac.liu@h2o.ai>
2025-10-23 16:46:55 +1100

    fixed documentation

--- installation.sh
+++ installation.sh
@@ -2,6 +2,8 @@
 # shellcheck shell=dash
 # shellcheck disable=SC2039  # local is non-POSIX
 #
+# Copyright 2025 H2O.ai, Inc.
+#
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
